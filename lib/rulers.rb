35db6ba9ce3e18f2445e54a37dbcfd235de29130
Miles Starkenburg <milesstarkenburg@gmail.com>
2014-02-17 06:44:22 -0800

    wip

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,8 +1,9 @@
 require "rulers/version"
-
+require 'pry'
 module Rulers
   class Application
     def call(env)
+      binding.pry
       'echo debug > debug.txt';
       [200, {'Content-Type' => 'text/html'},
       ["Hello from Rulers!"]]
