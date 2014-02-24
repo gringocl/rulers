ee0d6ae74794337d33687ff8d134010cca9387ad
Miles Starkenburg <milesstarkenburg@gmail.com>
2014-02-24 14:16:00 -0800

    wip

--- lib/rulers/dependencies.rb
+++ lib/rulers/dependencies.rb
@@ -1,12 +1,14 @@
+require 'pry'
 class Object
-  # def self.const_missing(c)
-  #   return nil if @calling_cont_missing
-  #
-  #   @calling_cont_missing = true
-  #   require Rulers.to_underscore(c.to_s)
-  #   klass = Object.const_get(c)
-  #   @calling_cont_missing = false
-  #
-  #   klass
-  # end
+  def self.const_missing(c)
+    binding.pry
+    return nil if @calling_cont_missing
+
+    @calling_cont_missing = true
+    require Rulers.to_underscore(c.to_s)
+    klass = Object.const_get(c)
+    @calling_cont_missing = false
+
+    klass
+  end
 end
