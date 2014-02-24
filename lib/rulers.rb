f3f71f5968cb78465e321289bc5afb8e1432479e
Miles Starkenburg <milesstarkenburg@gmail.com>
2014-02-23 18:07:09 -0800

    Added default root route handling

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -7,9 +7,20 @@ module Rulers
   class Application
     def call(env)
       # binding.pry
+      if env['PATH_INFO'] == '/favicon.ico'
+        return [404, {'Content-Type'  => 'text/html'}, []]
+      elsif env['PATH_INFO'] == '/'
+        klass = Object.const_get('HomeController')
+        controller = klass.new(env)
+        text = controller.send('index')
+        return [200, {'Content-Type' => 'text/html'}, [text]]
+      end
       klass, act = get_controller_and_action(env)
       controller = klass.new(env)
-      text = controller.send(act)
+      begin
+        text = controller.send(act)
+      rescue
+      end
       [200, {'Content-Type' => 'text/html'},
       [text]]
     end
